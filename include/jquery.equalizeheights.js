8acbc7c61f84da28d8639b350309459407d3d77f
Dan Bettles <dan@danbettles.net>
2012-01-07 14:46:23 +0000

    The function is assigned directly to jQuery.fn - there is no longer a need to use jQuery.extend()

--- include/jquery.equalizeheights.js
+++ include/jquery.equalizeheights.js
@@ -6,43 +6,37 @@
  * @author Dan Bettles <dan@archaichorizon.com>
  */
 
-jQuery.extend(true, {
+/**
+ * @param {Object} [options]
+ * @return jQuery
+ */
+jQuery.fn.equalizeHeights = function (options) {
+    var actualOptions = options || {},
+        targetOuterHeight = 0;
 
-    fn: {
+    this.each(function () {
+        var currOuterHeight = jQuery(this).outerHeight(true);
 
-        /**
-         * @param {Object} [options]
-         * @return jQuery
-         */
-        equalizeHeights: function (options) {
-            var actualOptions = options || {},
-                targetOuterHeight = 0;
-
-            this.each(function () {
-                var currOuterHeight = jQuery(this).outerHeight(true);
-
-                if (currOuterHeight > targetOuterHeight) {
-                    targetOuterHeight = currOuterHeight;
-                }
-            });
-
-            if ((typeof actualOptions.minHeight !== 'undefined') && (targetOuterHeight < actualOptions.minHeight)) {
-                targetOuterHeight = actualOptions.minHeight;
-            }
-
-            this.each(function () {
-                var oThisEl = jQuery(this),
-                    currOuterHeight = jQuery(this).outerHeight(true);
-
-                if (currOuterHeight === targetOuterHeight) {
-                    return true;
-                }
-
-                oThisEl.height(targetOuterHeight - (currOuterHeight - oThisEl.height()));
-                return true;
-            });
-
-            return this;
+        if (currOuterHeight > targetOuterHeight) {
+            targetOuterHeight = currOuterHeight;
         }
+    });
+
+    if ((typeof actualOptions.minHeight !== 'undefined') && (targetOuterHeight < actualOptions.minHeight)) {
+        targetOuterHeight = actualOptions.minHeight;
     }
-});
+
+    this.each(function () {
+        var oThisEl = jQuery(this),
+            currOuterHeight = jQuery(this).outerHeight(true);
+
+        if (currOuterHeight === targetOuterHeight) {
+            return true;
+        }
+
+        oThisEl.height(targetOuterHeight - (currOuterHeight - oThisEl.height()));
+        return true;
+    });
+
+    return this;
+};
